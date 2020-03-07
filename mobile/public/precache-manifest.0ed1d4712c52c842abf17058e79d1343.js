219828051084a1f3a5ba69ad52449f6621ea0b42
17mcpc14 <17mcpc14@uohyd.ac.in>
2020-03-07 13:00:32 +0530

    Mobile App

---
+++ mobile/public/precache-manifest.0ed1d4712c52c842abf17058e79d1343.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b982ff7a16e3e1664c8d548271437a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38da1cf6001142656daa",
+    "url": "/static/css/main.6e295437.chunk.css"
+  },
+  {
+    "revision": "fc0d377873f3b34e9934",
+    "url": "/static/js/10.3e516afb.chunk.js"
+  },
+  {
+    "revision": "09094bbe101170f6359e",
+    "url": "/static/js/11.308989a6.chunk.js"
+  },
+  {
+    "revision": "e78f814aa7a462e0fbd7",
+    "url": "/static/js/12.8a62ff10.chunk.js"
+  },
+  {
+    "revision": "d74069c52b0a560654d7acf5aae4dab5",
+    "url": "/static/js/12.8a62ff10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59b0ed1ebfa75426b7f",
+    "url": "/static/js/2.952ee5dd.chunk.js"
+  },
+  {
+    "revision": "84f3a7de0a2f6fb73625a58661a033f1",
+    "url": "/static/js/2.952ee5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c0f2fb4673b7b909c2",
+    "url": "/static/js/3.7d205a51.chunk.js"
+  },
+  {
+    "revision": "e329a9821b37d9e96fb5",
+    "url": "/static/js/4.409f9811.chunk.js"
+  },
+  {
+    "revision": "d74069c52b0a560654d7acf5aae4dab5",
+    "url": "/static/js/4.409f9811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d53c3e2966f6f9bc25",
+    "url": "/static/js/5.4af447ad.chunk.js"
+  },
+  {
+    "revision": "cd4d4bc5889404e81d53",
+    "url": "/static/js/6.40c069d3.chunk.js"
+  },
+  {
+    "revision": "74bd65e33720fc7b5762",
+    "url": "/static/js/7.58d8e671.chunk.js"
+  },
+  {
+    "revision": "9c6a78b85fcdd49e99a4",
+    "url": "/static/js/8.a00cb74b.chunk.js"
+  },
+  {
+    "revision": "c3dd1112755033127083",
+    "url": "/static/js/9.bd23a125.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/static/js/9.bd23a125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38da1cf6001142656daa",
+    "url": "/static/js/main.2ab0d9ff.chunk.js"
+  },
+  {
+    "revision": "6b9870c1d2a6089f53f8",
+    "url": "/static/js/runtime-main.f46ad4a0.js"
+  },
+  {
+    "revision": "ed79d5c5899810292473e915e94132a5",
+    "url": "/static/media/logo.ed79d5c5.png"
+  }
+]);
